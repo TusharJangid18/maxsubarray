7bc56fc7ef804327224a2a8dd423625319df6b73
Tushar <tusharjand1876@gmail.com>
2026-03-17 03:19:37 +0530

    Add ArrayCC.java

--- ArrayCC.java
+++ ArrayCC.java
@@ -1,40 +1,26 @@
 import java.util.*;
 public class ArrayCC {
-    public static void maxsubarray(int numbers[]){
-        int currsum=0;
-        int maxsum=Integer.MIN_VALUE;
-        int prefixsum[]=new int[numbers.length];
-        prefixsum[0] =numbers[0];
-        for(int i=1;i<prefixsum.length;i++){
-            prefixsum[i]=prefixsum[i-1]+numbers[i];
+    public static int trappedRainwater(int height[]) {
+        int n = height.length;
+        int leftMax[] = new int[n];
+        leftMax [0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
-        for(int i=0;i<numbers.length;i++){
-            int start=i;
-            for(int j=i;j<numbers.length;j++){
-                int end=j;
-                currsum=start==0?prefixsum[end]:prefixsum[end]-prefixsum[start-1]; 
-                if(maxsum<currsum){
-                    maxsum=currsum; 
-                }
-            }
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
-        System.out.println("max sum is "+maxsum);
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += Math.max(0, waterLevel - height[i]);
+        }
+        return trappedWater;
     }
-    public static void kadane(int numbers[]){
-        int currsum=0;
-        int maxsum=Integer.MIN_VALUE;
-        for(int i=0;i<numbers.length;i++){
-            currsum+=numbers[i];
-            if(currsum<0){
-                currsum=0;
-            }
-            maxsum=Math.max(currsum, maxsum);
-        }
-        System.out.println("max sum is "+maxsum);
-    }
-    public static void main(String args[]){
-        int numbers[]={-2,-3,4,-1,-2,1,5,-3};
-        maxsubarray(numbers);
-        kadane(numbers);
+    public static void main(String[] args) {
+        int[] height = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println(trappedRainwater(height));
     }
 }
